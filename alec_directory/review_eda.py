868e529f4bbc958b2c4e93788f0d44170808340a
Alec Kulakowski <alecthekulak@gmail.com>
2018-04-23 11:39:21 -0400

    added subdirectory for my work, started a EDS .py file (to later be turned into a .ipynb or something
    Signed-off-by: Alec Kulakowski <alecthekulak@gmail.com>

--- alec_directory/review_eda.py
+++ alec_directory/review_eda.py
@@ -11,4 +11,19 @@ validation = pd.read_csv(zf.open('Hoboken_restaurants_reviews.csv'))
 validation = validation.drop(columns=validation.columns.values[0:2]) # Drop index columns
 # Convert user_ratings from string to integer
 validation['user_rating'] = validation['user_rating'].apply(lambda x: int(x[0]))
+# Display price distribution
+print(validation['restaurant_price'].value_counts())
+# Display number of absent prices
+print('Missing prices: '+str(validation['restaurant_price'].isnull().sum()))
+#
+b = validation.itertuples()
+a = [x for x in b]
+w = a[0]
+for a in validation.itertuples():
+    # print(a)
+    if a.__getattribute__('restaurant_price') is None:
+    # if a['restaurant_price'].isnull():
+        print(a)
+validation['restaurant_price'].isnull().sum()
+validation['restaurant_price'].apply(lambda x: x.count('$'))
 validation.head(3) 
