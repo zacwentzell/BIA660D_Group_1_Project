31909c56a13527754a58b8ae8b408870eaafbaa4
Alec Kulakowski <alecthekulak@gmail.com>
2018-04-23 12:38:20 -0400

    Fixed missing values within dataset
    Signed-off-by: Alec Kulakowski <alecthekulak@gmail.com>

--- alec_directory/review_eda.py
+++ alec_directory/review_eda.py
@@ -9,21 +9,36 @@ import pandas as pd
 zf = ZipFile("Hoboken_restaurants_reviews.csv.zip")
 validation = pd.read_csv(zf.open('Hoboken_restaurants_reviews.csv'))
 validation = validation.drop(columns=validation.columns.values[0:2]) # Drop index columns
-# Convert user_ratings from string to integer
+# Convert user_ratings from string to integer and restaurant ratings from string to float
 validation['user_rating'] = validation['user_rating'].apply(lambda x: int(x[0]))
+validation['restaurant_rating'] = validation['restaurant_rating'].apply(lambda x: float(x[0:3]))
 # Display price distribution
 print(validation['restaurant_price'].value_counts())
 # Display number of absent prices
 print('Missing prices: '+str(validation['restaurant_price'].isnull().sum()))
+# Replace missing values with mean and convert to integer
+def try_convert(x, y=0):
+    try: return(x.count('$'))
+    except: return(y)
+average_price = sum(validation['restaurant_price'].apply(lambda x: try_convert(x))) / validation['restaurant_price'].value_counts().sum()
+validation['restaurant_price'] = validation['restaurant_price'].apply(lambda x: try_convert(x, y=average_price))
 #
+from sklearn.preprocessing import
+
+validation.head(3)
+
+
+
 b = validation.itertuples()
 a = [x for x in b]
+a[5271:5274]
 w = a[0]
-for a in validation.itertuples():
-    # print(a)
-    if a.__getattribute__('restaurant_price') is None:
-    # if a['restaurant_price'].isnull():
-        print(a)
-validation['restaurant_price'].isnull().sum()
-validation['restaurant_price'].apply(lambda x: x.count('$'))
-validation.head(3) 
+w.__getattribute__('restaurant_price')
+# for a in validation.itertuples():
+#     # print(a)
+#     if a.__getattribute__('restaurant_price') is None:
+#     # if a['restaurant_price'].isnull():
+#         print(a)
+# validation['restaurant_price'].isnull().sum()
+# validation['restaurant_price'].apply(lambda x: x.count('$'))
+# validation.head(3)
