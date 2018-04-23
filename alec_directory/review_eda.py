9a853c96c41a34f5beb94751acd8a0b70861e2f2
Alec Kulakowski <alecthekulak@gmail.com>
2018-04-23 11:20:31 -0400

    added subdirectory for my work, started a EDS .py file (to later be turned into a .ipynb or something
    Signed-off-by: Alec Kulakowski <alecthekulak@gmail.com>

--- alec_directory/review_eda.py
+++ alec_directory/review_eda.py
@@ -1,21 +1,14 @@
 ### Review Data Exploratory Data Analysis
 
 # Navigate into the correct project directory
-import os
+import os # os.listdir()
 os.chdir('../BIA660D_Group_1_Project')
 # Extract the review data from its .zip form
 from zipfile import ZipFile
 import pandas as pd
 zf = ZipFile("Hoboken_restaurants_reviews.csv.zip")
 validation = pd.read_csv(zf.open('Hoboken_restaurants_reviews.csv'))
-validation.drop(columns=[0])
-validation.head(3)
-
-# zf = ZipFile("Hoboken_restaurants_reviews.csv.zip")
-# validation = zf.read(zf.namelist()[0])
-# import zipfile
-# a = zipfile.ZipFile("Hoboken_restaurants_reviews.csv.zip", "r")
-# a.infolist()[0]
-# a.infolist()
-# a.namelist()
-# a.open(a.infolist()[0])
+validation = validation.drop(columns=validation.columns.values[0:2]) # Drop index columns
+# Convert user_ratings from string to integer
+validation['user_rating'] = validation['user_rating'].apply(lambda x: int(x[0]))
+validation.head(3) 
