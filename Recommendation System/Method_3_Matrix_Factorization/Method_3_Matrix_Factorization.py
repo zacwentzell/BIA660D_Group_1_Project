33f2d8751ddaf243991956af1228c68d3fd0419e
Mani <nman@stevens.edu>
2018-04-26 21:54:57 -0400

    debug

--- Recommendation System/Method_3_Matrix_Factorization/Method_3_Matrix_Factorization.py
+++ Recommendation System/Method_3_Matrix_Factorization/Method_3_Matrix_Factorization.py
@@ -18,17 +18,28 @@ def get_recommender_df(df, W, H):
     predict_matrix = np.matrix(np.dot(W,H))
     result_df = pd.DataFrame(predict_matrix)
     result_df.columns = df.columns[1:]
-    result_df.index = df.iloc[:,0]
+    result_df.insert(0, 'user_id', train_set.iloc[:, 0])
     return result_df
 
-def export_result(df, result_df, n_top):
+def export_result(df, result_df, top_n):
     recommendation_li = []
     for i in range(len(result_df)):
-        recommendation_top_n = list(result_df.iloc[i,:].sort_values(ascending=False).head(n_top).index)
+        attended_res_li = None
+        try:
+            attended_res_li = list(df.iloc[i, 1:][train_set.iloc[i, 1:] != 0].index)
+        except:
+            pass
+        if attended_res_li is not None:
+            for res in attended_res_li:
+                result_df.loc[i, res] = 0
+
+        recommendation_top_n = list(result_df.iloc[i, 1:].sort_values(ascending=False).head(top_n).index)
         recommendation_li.append(recommendation_top_n)
+
     export_df = pd.DataFrame(df.user_id)
     export_df['Recommendation'] = recommendation_li
     export_df.to_csv('matrix_factorization_result.csv')
+
     return None
 
 def main():
