2b19a13c25785b8e7682d35b7a515956595d1def
Mani <nman@stevens.edu>
2018-04-27 00:42:44 -0400

    debug

--- Recommendation System/Method_2_Collaborative_Filtering/Method_2_Collaborative_Filtering.py
+++ Recommendation System/Method_2_Collaborative_Filtering/Method_2_Collaborative_Filtering.py
@@ -2,8 +2,6 @@ import pandas as pd
 import graphlab
 from sklearn.model_selection import train_test_split
 
-
-
 def get_train_test(file_name):
     df = pd.read_csv(file_name)
     train_data, test_data = train_test_split(df, test_size=0.25)
@@ -34,12 +32,12 @@ def get_result(export_name, model, top_n=3):
     return None
 
 def main():
-    train, test = get_train_test('Method_2_dataset.csv')
-    model = get_similarity_model(train, 'cosine')
-    #model = get_similarity_model(train, 'jaccard')
+    train, test = get_train_test('method_2_dataset.csv')
+    #model = get_similarity_model(train, 'cosine')
+    model = get_similarity_model(train, 'jaccard')
 
-    get_result('Cosine_recommendation_result.csv', model, 3)
-    #get_result('Jaccard_recommendation_result.csv', model, 3)
+    #get_result('Cosine_recommendation_result.csv', model, 3)
+    get_result('Jaccard_recommendation_result.csv', model, 3)
     return None
 
 if __name__ == '__main__':
