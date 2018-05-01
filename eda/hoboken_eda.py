7fbcfd105015173d07878f7ffe18b2f1bc4927a3
Alec Kulakowski <alecthekulak@gmail.com>
2018-05-01 01:08:51 -0400

    Work done on the data cleaning and preprocessing
    hoboken_eda work, cleaning and processing the data scraped from Hoboken
    
    Signed-off-by: Alec Kulakowski <alecthekulak@gmail.com>

--- eda/hoboken_eda.py
+++ eda/hoboken_eda.py
@@ -2,33 +2,18 @@
 Clean and EDA the Hoboken review data
 BIA660D - Group 1: Alec Kulakowski
 """
+# Import Statements
 import pandas as pd
-hoboken_raw = pd.read_csv('../BIA660D_Group_1_Project/scraping/hoboken_cleaned.csv')
-hoboken_raw.head(2)
-# Lets first move all user data, since this isn't the reccomendation system and user data would just help
-# overfit the model.
-hoboken_raw.drop(columns=['user_id', 'user_name'], inplace=True)
-hoboken_raw.head(2)
-# Now lets see if we can transform those restaurant ratings into ratings that are independent of the review in each
-#row, thus they can be used as predictive variables without using look-ahead bias (although to completely avoid such
-#bias, we'd need to know the mean review for the restaurant at the time of each review, which could be done manually
-#post scraping if scraping had included review date or order.
-restaurant_list = hoboken_raw['restaurant_name'].unique()
-for restaurant in restaurant_list:
-    review_indexes = hoboken_raw.loc[hoboken_raw['restaurant_name'] == restaurant].index
-    review_count = len(review_indexes)
-    superscore = review_count * hoboken_raw.iloc[review_indexes[0]]['restaurant_rating']
-    for i in review_indexes:
-        review = hoboken_raw.iloc[i]
-        review.loc['restaurant_rating'] = (superscore - review['user_rating']) / (review_count - 1)
-        hoboken_raw.iloc[i] = review
-        print("Review #"+str(i)+" done.")
-# hoboken_new = hoboken_raw.copy()
-hoboken_raw.to_csv('../BIA660D_Group_1_Project/eda/hoboken_step1.csv')
-#########################
+# import matplotlib.pyplot as plt #dont run this in pycharm
+data = pd.read_csv('../BIA660D_Group_1_Project/eda/hoboken_step1.csv')
+# data.drop(columns=data.columns[0], inplace=True)
+# Deal with missing data
+data['restaurant_rating'].fillna(data['restaurant_rating'].mean(), inplace=True)
+data.head(2)
+# Explore distribution of
+# data['restaurant_rating'].plot(type='')
+rating_distribution = data['user_rating'].value_counts().loc[list(range(1,6))]
+data['restaurant_type'] = data['restaurant_type'].map(lambda x: x.replace("'",'').replace('[','').replace(']','').split(','))
+all_types = set()
+for types in data['restaurant_type']:
 
-hoboken_raw.head()
-# TODO: Remove
-restaurant = restaurant_list[3]
-i = review_indexes[3]
-hoboken_raw.head()
