d9bd824569cf16faff0e7b8054697333df8351c9
Alec Kulakowski <alecthekulak@gmail.com>
2018-05-01 18:41:12 -0400

    Using bag-of-words and bag-of-bigrams to predict ratings
    Also README edits, basic preprocessing within scripts, moved on to BOW and bigram predictors (both classifiers and regressors)
    
    Signed-off-by: Alec Kulakowski <alecthekulak@gmail.com>

--- prediction/bagofwords.py
+++ prediction/bagofwords.py
@@ -21,7 +21,7 @@ vectorizer.fit(train_reviews)
 # len(vectorizer.vocabulary_)
 train_bow = vectorizer.transform(train_reviews)
 test_bow = vectorizer.transform(test_reviews)
-# Now with bag-of-bigrams
+# Now with bag-of-bigrams #ngram_range=(1,2) # changed to (2)
 bigram_vectorizer = CountVectorizer(strip_accents='ascii', stop_words='english', binary=True, max_df=0.9, max_features=1000, ngram_range=(1,2))
 bigram_vectorizer.fit(train_reviews)
 len(bigram_vectorizer.vocabulary_)
@@ -38,6 +38,11 @@ mlp.fit(train_bow, train_ratings)
 # regressor = MLPRegressor(tol=tolerance, hidden_layer_sizes=network_shape, random_state=r)
 regressor = MLPRegressor(**hyperparameters)
 regressor.fit(train_bow, train_ratings)
+# Record and desplay results
+nn_classifier_bow_train = mlp.score(train_bow, train_ratings)
+nn_classifier_bow_test = mlp.score(test_bow, test_ratings)
+nn_regressor_bow_train = regressor.score(train_bow, train_ratings)
+nn_regressor_bow_test = regressor.score(test_bow, test_ratings)
 print('Bag-of-Words:')
 print('Classifier Results: '+per(mlp.score(train_bow, train_ratings))+' training accuracy, '+per(mlp.score(test_bow, test_ratings))+' testing accuracy')
 print('Regressor Results: '+per(regressor.score(train_bow, train_ratings))+' training accuracy, '+per(regressor.score(test_bow, test_ratings))+' testing accuracy')
@@ -46,8 +51,76 @@ mlp2 = MLPClassifier(**hyperparameters)
 regressor2 = MLPRegressor(**hyperparameters)
 mlp2.fit(train_bigram, train_ratings)
 regressor2.fit(train_bigram, train_ratings)
+# Record and desplay results
+nn_classifier_bigram_train = mlp2.score(train_bigram, train_ratings)
+nn_classifier_bigram_test = mlp2.score(test_bigram, test_ratings)
+nn_regressor_bigram_train = regressor2.score(train_bigram, train_ratings)
+nn_regressor_bigram_test = regressor2.score(test_bigram, test_ratings)
 print('Bag-of-Bigrams:')
 print('Classifier Results: '+per(mlp2.score(train_bigram, train_ratings))+' training accuracy, '+per(mlp2.score(test_bigram, test_ratings))+' testing accuracy')
 print('Regressor Results: '+per(regressor2.score(train_bigram, train_ratings))+' training accuracy, '+per(regressor2.score(test_bigram, test_ratings))+' testing accuracy')
+# Now lets try using an SVM
+from sklearn.svm import LinearSVC
+svm = LinearSVC()
+svm2 = LinearSVC()
+svm.fit(train_bow, train_ratings)
+svm2.fit(train_bigram, train_ratings)
+# Record and desplay results
+svm_bow_train = svm.score(train_bow, train_ratings)
+svm_bow_test = svm.score(test_bow, test_ratings)
+svm_bigram_train = svm2.score(train_bigram, train_ratings)
+svm_bigram_test = svm2.score(test_bigram, test_ratings)
+print('SVM Results:')
+print('BOW Results: '+per(svm_bow_train)+' training accuracy, '+per(svm_bow_test)+' testing accuracy')
+print('Bigram Results: '+per(svm_bigram_train)+' training accuracy, '+per(svm_bigram_test)+' testing accuracy')
+# Now lets try using passive aggressive classifier:
+from sklearn.linear_model import PassiveAggressiveClassifier, PassiveAggressiveRegressor
+pac = PassiveAggressiveClassifier()
+pac2 = PassiveAggressiveClassifier()
+par = PassiveAggressiveRegressor()
+par2 = PassiveAggressiveRegressor()
+# Now fit
+pac.fit(train_bow, train_ratings)
+par.fit(train_bow, train_ratings)
+pac2.fit(train_bigram, train_ratings)
+par2.fit(train_bigram, train_ratings)
+# Record and desplay results
+pac_bow_train = pac.score(train_bow, train_ratings)
+pac_bow_test = pac.score(test_bow, test_ratings)
+pac_bigram_train = pac2.score(train_bigram, train_ratings)
+pac_bigram_test = pac2.score(test_bigram, test_ratings)
+par_bow_train = par.score(train_bow, train_ratings)
+par_bow_test = par.score(test_bow, test_ratings)
+par_bigram_train = par2.score(train_bigram, train_ratings)
+par_bigram_test = par2.score(test_bigram, test_ratings)
+# Results
+print('Passive Aggressive Classifier')
+print('BOW Results: '+per(pac_bow_train)+' training accuracy, '+per(pac_bow_test)+' testing accuracy')
+print('Bigram Results: '+per(pac_bigram_train)+' training accuracy, '+per(pac_bigram_test)+' testing accuracy')
+print('Passive Aggressive Regressor')
+print('BOW Results: '+per(par_bow_train)+' training accuracy, '+per(par_bow_test)+' testing accuracy')
+print('Bigram Results: '+per(par_bigram_test)+' training accuracy, '+per(par_bigram_test)+' testing accuracy')
+# Lets try linear
+from sklearn.linear_model import LogisticRegression
+logistic = LogisticRegression(solver='sag', random_state=1, max_iter=500)
+logistic2 = LogisticRegression(solver='sag', random_state=1, max_iter=500)
+logistic.fit(train_bow, train_ratings)
+logistic2.fit(train_bigram, train_ratings)
+log_bow_train = logistic.score(train_bow, train_ratings)
+log_bow_test = logistic.score(test_bow, test_ratings)
+log_bigram_train = logistic2.score(train_bigram, train_ratings)
+log_bigram_test = logistic2.score(test_bigram, test_ratings)
+print('BOW: train:'+per(log_bow_train)+', test:'+per(log_bow_test))
+print('Bigram: train:'+per(log_bigram_train)+', test:'+per(log_bigram_test))
 
-
+# now for results table
+models = ['NN_Classifier', 'NN_Regressor', 'SVM', 'PA_Classifier', 'PA_Regressor']
+results = [['Bag-of-Words Training', 'Bag-of-Words Test', 'Bag-of-Bigrams Training', 'Bag-of-Bigrams Test'],
+           [nn_classifier_bow_train, nn_classifier_bow_test, nn_classifier_bigram_train, nn_classifier_bigram_test],
+           [nn_regressor_bow_train, nn_regressor_bow_test, nn_regressor_bigram_train, nn_regressor_bigram_test],
+           [svm_bow_train, svm_bow_test, svm_bigram_train, svm_bigram_test],
+           [pac_bow_train, pac_bow_test, pac_bigram_train, pac_bigram_test],
+           [par_bow_train, par_bow_test, par_bigram_train, par_bigram_test]]
+result_df = pd.DataFrame(results[1:], columns = results[0], index = models)
+result_df
+# Best results in training from:
