0dcd45b5a211ed4fd4d6a12bcb8adb9ff88afb35
Alec Kulakowski <alecthekulak@gmail.com>
2018-05-01 17:19:41 -0400

    Using bag-of-words and bag-of-bigrams to predict ratings
    Also README edits, basic preprocessing within scripts, moved on to BOW and bigram predictors (both classifiers and regressors)
    
    Signed-off-by: Alec Kulakowski <alecthekulak@gmail.com>

--- prediction/bagofwords.py
+++ prediction/bagofwords.py
@@ -13,7 +13,7 @@ reviews = data['user_text']
 ratings = data['user_rating']
 # Split the dataset into training and test data
 from sklearn.model_selection import train_test_split
-train_reviews, test_reviews, train_ratings, test_ratings = train_test_split(reviews, ratings, test_size=0.2, random_state=1)
+train_reviews, test_reviews, train_ratings, test_ratings = train_test_split(reviews, ratings, test_size=0.15, random_state=1)
 # Vectorize using bag-of-words
 from sklearn.feature_extraction.text import CountVectorizer
 vectorizer = CountVectorizer(strip_accents='ascii', stop_words='english', binary=True, max_df=0.9, max_features=1000)
@@ -27,23 +27,27 @@ bigram_vectorizer.fit(train_reviews)
 len(bigram_vectorizer.vocabulary_)
 train_bigram = bigram_vectorizer.transform(train_reviews)
 test_bigram = bigram_vectorizer.transform(test_reviews)
-# Now to fit models
+### Now to fit models
 from sklearn.neural_network import MLPClassifier, MLPRegressor
+# Define parameters & misc functions
+hyperparameters = {'tol': 0.005, 'hidden_layer_sizes': (250, 100, 50), 'random_state': 1}
 def per(x): return(str(round(100*x,2))+'%')
-mlp = MLPClassifier(tol=0.0005, random_state=1)
+mlp = MLPClassifier(**hyperparameters)
 mlp.fit(train_bow, train_ratings)
 # Try with regressor
-regressor = MLPRegressor(tol=0.0005, random_state=1)
+# regressor = MLPRegressor(tol=tolerance, hidden_layer_sizes=network_shape, random_state=r)
+regressor = MLPRegressor(**hyperparameters)
 regressor.fit(train_bow, train_ratings)
 print('Bag-of-Words:')
 print('Classifier Results: '+per(mlp.score(train_bow, train_ratings))+' training accuracy, '+per(mlp.score(test_bow, test_ratings))+' testing accuracy')
 print('Regressor Results: '+per(regressor.score(train_bow, train_ratings))+' training accuracy, '+per(regressor.score(test_bow, test_ratings))+' testing accuracy')
 # Oh, those results are sad, let's do it with bag-of-bigrams
-mlp = MLPClassifier(tol=0.0005, random_state=1)
-regressor = MLPRegressor(tol=0.0005, random_state=1)
-mlp.fit(train_bigram, train_ratings)
-regressor.fit(train_bigram, train_ratings)
+mlp2 = MLPClassifier(**hyperparameters)
+regressor2 = MLPRegressor(**hyperparameters)
+mlp2.fit(train_bigram, train_ratings)
+regressor2.fit(train_bigram, train_ratings)
 print('Bag-of-Bigrams:')
-print('Classifier Results: '+per(mlp.score(train_bigram, train_ratings))+' training accuracy, '+per(mlp.score(test_bigram, test_ratings))+' testing accuracy')
-print('Regressor Results: '+per(regressor.score(train_bigram, train_ratings))+' training accuracy, '+per(regressor.score(test_bigram, test_ratings))+' testing accuracy')
+print('Classifier Results: '+per(mlp2.score(train_bigram, train_ratings))+' training accuracy, '+per(mlp2.score(test_bigram, test_ratings))+' testing accuracy')
+print('Regressor Results: '+per(regressor2.score(train_bigram, train_ratings))+' training accuracy, '+per(regressor2.score(test_bigram, test_ratings))+' testing accuracy')
+
 
