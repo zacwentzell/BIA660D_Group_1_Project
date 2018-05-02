51d4f959369c060e2e693e46cda872a734c3896e
Alec Kulakowski <alecthekulak@gmail.com>
2018-05-02 12:55:45 -0400

    Finished bagofwords.py
    Ended with a paragraph describing score comparisons, still running script to add the result .csv to the appropriate subdirectory. A python notebook on this will likely follow shortly. Output .csv is attached, KNN classifiers and regressors had to be abandoned because of repeated memory issues despite troubleshooting in multiple ways.
    
    Signed-off-by: Alec Kulakowski <alecthekulak@gmail.com>

--- prediction/bagofwords.py
+++ prediction/bagofwords.py
@@ -11,9 +11,11 @@ data.head(3)
 # data.head(3)
 reviews = data['user_text']
 ratings = data['user_rating']
+del data
 # Split the dataset into training and test data
 from sklearn.model_selection import train_test_split
 train_reviews, test_reviews, train_ratings, test_ratings = train_test_split(reviews, ratings, test_size=0.15, random_state=1)
+del reviews, ratings
 # Vectorize using bag-of-words
 from sklearn.feature_extraction.text import CountVectorizer
 vectorizer = CountVectorizer(strip_accents='ascii', stop_words='english', binary=True, max_df=0.9, max_features=1000)
@@ -27,6 +29,7 @@ bigram_vectorizer.fit(train_reviews)
 len(bigram_vectorizer.vocabulary_)
 train_bigram = bigram_vectorizer.transform(train_reviews)
 test_bigram = bigram_vectorizer.transform(test_reviews)
+del vectorizer, bigram_vectorizer
 ### Now to fit models
 from sklearn.neural_network import MLPClassifier, MLPRegressor
 # Define parameters & misc functions
@@ -43,7 +46,8 @@ nn_classifier_bow_train = mlp.score(train_bow, train_ratings)
 nn_classifier_bow_test = mlp.score(test_bow, test_ratings)
 nn_regressor_bow_train = regressor.score(train_bow, train_ratings)
 nn_regressor_bow_test = regressor.score(test_bow, test_ratings)
-mlp = regressor = 1
+# mlp = regressor = 1
+del mlp, regressor
 print('Bag-of-Words:')
 print('Classifier Results: '+per(nn_classifier_bow_train)+' training accuracy, '+per(nn_classifier_bow_test)+' testing accuracy')
 print('Regressor Results: '+per(nn_regressor_bow_train)+' training accuracy, '+per(nn_regressor_bow_test)+' testing accuracy')
@@ -57,7 +61,8 @@ nn_classifier_bigram_train = mlp2.score(train_bigram, train_ratings)
 nn_classifier_bigram_test = mlp2.score(test_bigram, test_ratings)
 nn_regressor_bigram_train = regressor2.score(train_bigram, train_ratings)
 nn_regressor_bigram_test = regressor2.score(test_bigram, test_ratings)
-mlp2 = regressor2 = 1
+# mlp2 = regressor2 = None
+del mlp2, regressor2
 print('Bag-of-Bigrams:')
 print('Classifier Results: '+per(nn_classifier_bigram_train)+' training accuracy, '+per(nn_classifier_bigram_test)+' testing accuracy')
 print('Regressor Results: '+per(nn_regressor_bigram_train)+' training accuracy, '+per(nn_regressor_bigram_test)+' testing accuracy')
@@ -72,7 +77,8 @@ svm_bow_train = svm.score(train_bow, train_ratings)
 svm_bow_test = svm.score(test_bow, test_ratings)
 svm_bigram_train = svm2.score(train_bigram, train_ratings)
 svm_bigram_test = svm2.score(test_bigram, test_ratings)
-svm = svm2 = 1
+# svm = svm2 = None
+del svm, svm2
 print('SVM Results:')
 print('BOW Results: '+per(svm_bow_train)+' training accuracy, '+per(svm_bow_test)+' testing accuracy')
 print('Bigram Results: '+per(svm_bigram_train)+' training accuracy, '+per(svm_bigram_test)+' testing accuracy')
@@ -96,7 +102,8 @@ par_bow_train = par.score(train_bow, train_ratings)
 par_bow_test = par.score(test_bow, test_ratings)
 par_bigram_train = par2.score(train_bigram, train_ratings)
 par_bigram_test = par2.score(test_bigram, test_ratings)
-pac = par = pac2 = par2 = 1
+# pac = par = pac2 = par2 = 1
+del pac, par, pac2, par2
 # Results
 print('Passive Aggressive Classifier')
 print('BOW Results: '+per(pac_bow_train)+' training accuracy, '+per(pac_bow_test)+' testing accuracy')
@@ -114,7 +121,8 @@ log_bow_train = logistic.score(train_bow, train_ratings)
 log_bow_test = logistic.score(test_bow, test_ratings)
 log_bigram_train = logistic2.score(train_bigram, train_ratings)
 log_bigram_test = logistic2.score(test_bigram, test_ratings)
-logistic = logistic2 = 1
+# logistic = logistic2 = 1
+del logistic, logistic2
 print('Logistic Regression:')
 print('BOW: train:'+per(log_bow_train)+', test:'+per(log_bow_test))
 print('Bigram: train:'+per(log_bigram_train)+', test:'+per(log_bigram_test))
@@ -128,13 +136,14 @@ rfc_bow_train = rfc.score(train_bow, train_ratings)
 rfc_bow_test = rfc.score(test_bow, test_ratings)
 rfc_bigram_train = rfc2.score(train_bigram, train_ratings)
 rfc_bigram_test = rfc2.score(test_bigram, test_ratings)
-rfc = rfc2 = 1
+# rfc = rfc2 = 1
+del rfc, rfc2
 print('Random Forest Classifier:')
 print('BOW: train:'+per(rfc_bow_train)+', test:'+per(rfc_bow_test))
 print('Bigram: train:'+per(rfc_bigram_train)+', test:'+per(rfc_bigram_test))
 #Regressor
 rfr = RandomForestRegressor(random_state=1, n_estimators=30)
-rfr2 = RandomForestRegressor(random_state=1, n_estimators=30)
+rfr2 = RandomForestRegressor(random_state=1, n_estimators=30) #n_jobs=-1
 rfr.fit(train_bow, train_ratings)
 rfr2.fit(train_bigram, train_ratings)
 rfr_bow_train = rfr.score(train_bow, train_ratings)
@@ -145,43 +154,50 @@ rfr = rfr2 = 1
 print('Random Forest Regressor:')
 print('BOW: train:'+per(rfr_bow_train)+', test:'+per(rfr_bow_test))
 print('Bigram: train:'+per(rfr_bigram_train)+', test:'+per(rfr_bigram_test))
-## KNN ###
-from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
-knc = KNeighborsClassifier()
-knc2 = KNeighborsClassifier()
-knc.fit(train_bow, train_ratings)
-knc2.fit(train_bigram, train_ratings)
-knc_bow_train = knc.score(train_bow, train_ratings)
-knc_bow_test = knc.score(test_bow, test_ratings)
-knc_bigram_train = knc2.score(train_bigram, train_ratings)
-knc_bigram_test = knc2.score(test_bigram, test_ratings)
-knc = knc2 = 1
-print('KNN Classifier:')
-print('BOW: train:'+per(knc_bow_train)+', test:'+per(knc_bow_test))
-print('Bigram: train:'+per(knc_bigram_train)+', test:'+per(knc_bigram_test))
-#regressor
-knr = KNeighborsRegressor()
-knr2 = KNeighborsRegressor()
-knr.fit(train_bow, train_ratings)
-knr2.fit(train_bigram, train_ratings)
-knr_bow_train = knr.score(train_bow, train_ratings)
-knr_bow_test = knr.score(test_bow, test_ratings)
-knr_bigram_train = knr2.score(train_bigram, train_ratings)
-knr_bigram_test = knr2.score(test_bigram, test_ratings)
-print('KNN Regressor:')
-print('BOW: train:'+per(knr_bow_train)+', test:'+per(knr_bow_test))
-print('Bigram: train:'+per(knr_bigram_train)+', test:'+per(knr_bigram_test))
+# rfr = rfr2 = None
+del rfr, rfr2
+#
+import gc
+gc.collect()
+## KNN ### #memory error, multiple, in future maybe use #RadiusNeighborsClassifier, and #weights='distance' for both
+# from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
+# knc = KNeighborsClassifier(algorithm='auto', n_jobs=-1)
+# knc2 = KNeighborsClassifier(algorithm='auto', n_jobs=-1)
+# knc.fit(train_bow, train_ratings)
+# knc2.fit(train_bigram, train_ratings)
+# knc_bow_train = knc.score(train_bow, train_ratings)
+# knc_bow_test = knc.score(test_bow , test_ratings)
+# knc_bigram_train = knc2.score(train_bigram, train_ratings)
+# knc_bigram_test = knc2.score(test_bigram, test_ratings)
+# del knc, knc2 #knc = knc2 = 1
+# print('KNN Classifier:')
+# print('BOW: train:'+per(knc_bow_train)+', test:'+per(knc_bow_test))
+# print('Bigram: train:'+per(knc_bigram_train)+', test:'+per(knc_bigram_test))
+#regressor #also memory error
+# knr = KNeighborsRegressor()
+# knr2 = KNeighborsRegressor()
+# knr.fit(train_bow, train_ratings)
+# knr2.fit(train_bigram, train_ratings)
+# knr_bow_train = knr.score(train_bow, train_ratings)
+# knr_bow_test = knr.score(test_bow, test_ratings)
+# knr_bigram_train = knr2.score(train_bigram, train_ratings)
+# knr_bigram_test = knr2.score(test_bigram, test_ratings)
+# del knr, knr2
+# print('KNN Regressor:')
+# print('BOW: train:'+per(knr_bow_train)+', test:'+per(knr_bow_test))
+# print('Bigram: train:'+per(knr_bigram_train)+', test:'+per(knr_bigram_test))
+gc.collect()
 ### Decision Tree
 from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
-dtc = DecisionTreeClassifier()
-dtc2 = DecisionTreeClassifier()
+dtc = DecisionTreeClassifier(random_state=1)
+dtc2 = DecisionTreeClassifier(random_state=1)
 dtc.fit(train_bow, train_ratings)
 dtc2.fit(train_bigram, train_ratings)
 dtc_bow_train = dtc.score(train_bow, train_ratings)
 dtc_bow_test = dtc.score(test_bow, test_ratings)
 dtc_bigram_train = dtc2.score(train_bigram, train_ratings)
 dtc_bigram_test = dtc2.score(test_bigram, test_ratings)
-dtc = dtc2 = 1
+del dtc, dtc2
 print('Decision Tree Classifier:')
 print('BOW: train:'+per(dtc_bow_train)+', test:'+per(dtc_bow_test))
 print('Bigram: train:'+per(dtc_bigram_train)+', test:'+per(dtc_bigram_test))
@@ -194,13 +210,14 @@ dtr_bow_train = dtr.score(train_bow, train_ratings)
 dtr_bow_test = dtr.score(test_bow, test_ratings)
 dtr_bigram_train = dtr2.score(train_bigram, train_ratings)
 dtr_bigram_test = dtr2.score(test_bigram, test_ratings)
-dtr = dtr2 = 1
+del dtr, dtr2 # dtr = dtr2 = 1
 print('Decision Tree Regressor:')
 print('BOW: train:'+per(dtr_bow_train)+', test:'+per(dtr_bow_test))
 print('Bigram: train:'+per(dtr_bigram_train)+', test:'+per(dtr_bigram_test))
 # now for results table
 models = ['NN_Classifier', 'NN_Regressor', 'SVM', 'PA_Classifier', 'PA_Regressor', 'Logistic',\
-          'RF_Classifier', 'RF_Regressor', 'KNN_Classifier', 'KNN_Regressor', 'DT_Classifier', 'DT_Regressor']
+          'RF_Classifier', 'RF_Regressor', 'DT_Classifier', 'DT_Regressor']
+                                          #'KNN_Classifier', 'KNN_Regressor',
 results = [['Bag-of-Words Training', 'Bag-of-Words Test', 'Bag-of-Bigrams Training', 'Bag-of-Bigrams Test'],
            [nn_classifier_bow_train, nn_classifier_bow_test, nn_classifier_bigram_train, nn_classifier_bigram_test],
            [nn_regressor_bow_train, nn_regressor_bow_test, nn_regressor_bigram_train, nn_regressor_bigram_test],
@@ -210,11 +227,12 @@ results = [['Bag-of-Words Training', 'Bag-of-Words Test', 'Bag-of-Bigrams Traini
            [log_bow_train, log_bow_test, log_bigram_train, log_bigram_test],
            [rfc_bow_train, rfc_bow_test, rfc_bigram_train, rfc_bigram_test],
            [rfr_bow_train, rfr_bow_test, rfr_bigram_train, rfr_bigram_test],
-           [knc_bow_train, knc_bow_test, knc_bigram_train, knc_bigram_test],
-           [knr_bow_train, knr_bow_test, knr_bigram_train, knr_bigram_test],
+           # [knc_bow_train, knc_bow_test, knc_bigram_train, knc_bigram_test],
+           # [knr_bow_train, knr_bow_test, knr_bigram_train, knr_bigram_test],
            [dtc_bow_train, dtc_bow_test, dtc_bigram_train, dtc_bigram_test],
            [dtr_bow_train, dtr_bow_test, dtr_bigram_train, dtr_bigram_test]]
 result_df = pd.DataFrame(results[1:], columns = results[0], index = models)
+result_df
 result_df.to_csv('../BIA660D_Group_1_Project/prediction/bagofwords.csv')
 # result_df = pd.DataFrame.from_csv('../BIA660D_Group_1_Project/prediction/bagofwords.csv')
 result_df
@@ -249,5 +267,8 @@ could assume a priori that they are a linear function. Review ratings have a neg
 at the ratings 4 and 5. Restaurant ratings also have a negative skew, but since they are a mean of a sample of the 
 previously discussed distribution, they are much more concentrated, with high frequencies for 3 and 4 and negligible 
 frequency for all other values. 
+
+Memory errors were encountered when using K-Nearest Neighbors Classifier and Regressor. I may try them again in a 
+document where they are used alone, perhaps also using some Radius Neighbor methods. 
 """
 # Best results in training from:
